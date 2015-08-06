27ac8d1f1f9bc0b72461b0363daa57202e25e2ab
邵银岭 <SYLApple@shaoyinlingdeMacBook-Air.local>
2015-08-06 22:49:15 +0800

    抽一个创建单例对象的宏的头文件

---
+++ ProjectEnter/ProjectEnter/Classes/Other(其他)/Single.h
@@ -0,0 +1,72 @@
+//
+//  Single.h
+//  ProjectEnter
+//
+//  Created by 邵银岭 on 15/8/6.
+//  Copyright (c) 2015年 邵银岭. All rights reserved.
+//  单例对象抽出为宏
+
+#ifndef ProjectEnter_Single_h
+#define ProjectEnter_Single_h
+
+#define interfaceSingle(name)  + (instancetype)share##name
+
+#if __has_feature(objc_arc)
+// 如果是ARC
+#define implementationSingle(name)  + (instancetype)share##name \
+{ \
+return [[self alloc] init]; \
+} \
+static id _instance; \
++ (instancetype)allocWithZone:(struct _NSZone *)zone \
+{ \
+static dispatch_once_t onceToken; \
+dispatch_once(&onceToken, ^{ \
+_instance = [super allocWithZone:zone]; \
+}); \
+return _instance; \
+} \
+- (id)copyWithZone:(NSZone *)zone \
+{ \
+return _instance; \
+} \
+- (id)mutableCopyWithZone:(NSZone *)zone \
+{ \
+return _instance; \
+}
+#else
+// 如果不是ARC
+#define implementationSingle(name)  + (instancetype)share##name \
+{ \
+return [[self alloc] init]; \
+} \
+static id _instance; \
++ (instancetype)allocWithZone:(struct _NSZone *)zone \
+{ \
+static dispatch_once_t onceToken; \
+dispatch_once(&onceToken, ^{ \
+_instance = [super allocWithZone:zone]; \
+}); \
+return _instance; \
+} \
+- (id)copyWithZone:(NSZone *)zone \
+{ \
+return _instance; \
+} \
+- (id)mutableCopyWithZone:(NSZone *)zone \
+{ \
+return _instance; \
+}\
+- (oneway void)release \
+{} \
+- (instancetype)retain \
+{ \
+return _instance; \
+} \
+- (NSUInteger)retainCount \
+{ \
+return MAXFLOAT; \
+}
+#endif
+
+#endif
